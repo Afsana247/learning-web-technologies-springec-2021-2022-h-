19de9e2ba31cc0425feb0e784fe3f23128ce289b
Afsana Mim <91478898+Afsana247@users.noreply.github.com>
2022-02-19 23:48:32 +0600

    html

---
+++ MID_LAB_TASK_06_PHP_SESSION/changepasswoed.php
@@ -0,0 +1,91 @@
+
+<html>
+<head>
+
+	<title>CHANGE PASSWORD</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<table border="1" width="100%">
+		<tr height="80px">
+			
+		<td>X Company</b>
+		<table align="Right">
+			<tr><td>Logged in as</td>
+			<td><ul><li><a href="">Bob</a>|</li></ul></td>
+			<td><ul><li><a href="">Logout</a>|</li></ul></td>
+			
+			</tr></table>
+		</td>
+	</tr>
+	
+	<tr height="200px">
+
+			<td>
+				<table border="" width="100%">
+					<tr>
+						<td>	<b>Account</b><hr><p><ul><li><a href="">Dashboard</a></li>
+				<li><a href="">View Profile</a></li>
+				<li><a href="">Edit Profile</a></li>
+				<li><a href="">Change Profilr</a></li>
+				<li><a href="">Change Password</a></li>
+				<li><a href="">Logout</a></li></ul></p></td>
+
+
+     <td>
+     	<fieldset>
+     		<legend><b>CHANGE PASSWORD</b></legend>
+     		<table>
+     			<tr>
+     				<td>Current Password</td>
+     				<td><input type="password" name="password"value=""></td>
+     			</tr>
+     			<tr>
+     				<td>New Password</td>
+     				<td><input type="password" name="password"value=""></td>
+     			</tr>
+     			<tr>
+     				<td>Retype New Password</td>
+     				<td><input type="password" name="password"value=""></td>
+     			</tr>
+  
+  	</table>
+     		<hr>
+     		<table>
+     		<tr>
+     				<td>
+     					<input type="submit" name="submit"value="Submit">
+     				</td>
+     			</tr>
+     		</table>
+     	
+     		
+     		
+     	
+     		
+     	</fieldset>
+
+
+
+
+
+
+
+
+			       </td>
+		</tr>
+</table>
+
+</td>
+	</tr>
+
+<tr height="50">
+
+			<td align="center">copyright@2017</td>
+	</tr>
+
+
+	</table>
+
+</body>
+</html>
